1f9710041996f2e693fe5e3c451487451662c857
AnniaBenitez <anniamicaela@gmail.com>
2024-07-20 13:51:05 -0400

    arreglo visual

--- front-end/src/pages/homePage/Home.jsx
+++ front-end/src/pages/homePage/Home.jsx
@@ -14,40 +14,28 @@ import SidebarItem from './components/SidebarItem';
 import LandingPage from './components/mainPage';
 
 // Este componente de barra lateral es para móvil y escritorio
-function Sidebar({ children, expanded, setExpanded }) {
+function Sidebar({ children }) {
   return (
     <aside
-      className={`fixed inset-y-0 ${expanded ? 'w-64' : 'w-20'} transition-all box-border h-full bg-white border-r shadow-sm`}
+      className={`fixed inset-y-0 w-5/6 sm:w-64 transition-all box-border h-full bg-white border-r shadow-sm z-400`}
     >
       <div className="flex h-full flex-col">
         <div className="flex items-center justify-between p-4 pb-2">
           <img
             src="https://masencarnacion.opentechla.com/themes/mas-encarnacion/assets/radio/img/logo_alt.png"
-            className={`overflow-hidden transition-all ${expanded ? 'w-32' : 'w-0'}`}
+            className={`overflow-hidden transition-all w-32`}
             alt=""
           />
-          <div className={`${expanded ? '' : 'hidden sm:block'}`}>
-            <button
-              onClick={() => setExpanded((curr) => !curr)}
-              className="rounded-lg bg-gray-50 p-1.5 hover:bg-gray-100"
-            >
-              {expanded ? (
-                <ArrowRight className="h-6 w-6" />
-              ) : (
-                <ArrowLeft className="h-6 w-6" />
-              )}
-            </button>
-          </div>
         </div>
         <ul className="flex-1 px-3">{children}</ul>
         <div className="flex border-t p-3">
           <img
             src="https://ui-avatars.com/api/?background=c7d2fe&color=3730a3&bold=true&name=Mark+Ruffalo"
             alt=""
-            className="h-10 w-10 rounded-md"
+            className="h-10 w-10 rounded-md mr-3"
           />
           <div
-            className={`flex items-center justify-between overflow-hidden transition-all ${expanded ? 'ml-3 w-52' : 'w-0'}`}
+            className={`flex items-center justify-between overflow-hidden transition-allml-3 w-52`}
           >
             <div className="leading-4">
               <h4 className="font-semibold">Carlos Lugo</h4>
@@ -70,7 +58,6 @@ function HomePageContent() {
 }
 
 export default function MakeSidebar() {
-  const [expanded, setExpanded] = useState(true);
   const navBarItems = [
     {
       icon: <Home />,
@@ -101,17 +88,14 @@ export default function MakeSidebar() {
     },
   ];
 
-  // Define el ancho del Sidebar basado en si está expandido
-  const sidebarWidth = expanded ? 'w-64' : 'w-20';
-
   return (
     <div className="flex">
-      <Sidebar expanded={expanded} setExpanded={setExpanded}>
+      <Sidebar >
         {navBarItems.map((item, index) => (
-          <SidebarItem key={index} expanded={expanded} {...item} />
+          <SidebarItem key={index} {...item} />
         ))}
       </Sidebar>
-      <main className={`flex-1 ml-${expanded ? '64' : '20'} p-6`}> {/* Ajusta el margen izquierdo según el estado del Sidebar */}
+      <main className="flex-1 ml-64 p-6"> {/* Ajusta el margen izquierdo según el ancho del Sidebar */}
         <HomePageContent />
       </main>
     </div>
